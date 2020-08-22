e75e34fa9dd4a14cddae4b4d932d2887211a1d27
LysakArtem <Perry.dev7@gmail.com>
2020-08-23 00:25:37 +0300

    first commit

---
+++ JS/task_5.js
@@ -0,0 +1,33 @@
+"use strict"
+
+
+const products = [
+    { name: 'Радар', price: 1300, quantity: 4 },
+    { name: 'Сканер', price: 2700, quantity: 3 },
+    { name: 'Дроид', price: 400, quantity: 7 },
+    { name: 'Захват', price: 1200, quantity: 2 },
+  ];
+  
+  const getAllPropValues = function(productsArr, prop) {
+      const selectProp=[];
+    for(const product of productsArr){
+        // console.log(product[prop]);
+        if(product[prop]){
+        selectProp.push(product[prop]);
+        }
+    }
+    return selectProp;
+  };
+  
+
+
+
+
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
+  
+  console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
+  
+  console.log(getAllPropValues(products, 'category')); // []
